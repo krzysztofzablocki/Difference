1e59a5624f938c8047d8bb0b06b324fbb97eb276
Benjamin Henshall <ben.henshall@babylonhealth.com>
2020-06-19 14:43:15 +0100

    Started implementing tests

--- Sources/Difference.swift
+++ Sources/Difference.swift
@@ -1,18 +1,55 @@
-//
-//  Difference.swift
-//  Difference
-//
-//  Created by Krzysztof Zablocki on 18.10.2017
-//  Copyright © 2017 Krzysztof Zablocki. All rights reserved.
-//
-
 import Foundation
 
 fileprivate extension String {
     init<T>(dumping object: T) {
         self.init()
         dump(object, to: &self)
+        self = withoutDumpArtifacts
     }
+
+    private var withoutDumpArtifacts: String {
+        self.replacingOccurrences(of: "- ", with: "")
+            .replacingOccurrences(of: "\n", with: "")
+    }
+}
+
+private func enumLabelFromFirstChild(_ mirror: Mirror) -> String? {
+    switch mirror.displayStyle {
+    case .enum: return mirror.children.first?.label
+    default: return nil
+    }
+}
+
+fileprivate extension Mirror {
+    var displayStyleDescriptor: String {
+        switch self.displayStyle {
+        case .enum: return "Enum"
+        default: return "Child"
+        }
+    }
+}
+
+fileprivate func handleChildlessEnum<T>(
+    _ expected: T,
+    _ received: T,
+    _ indentationLevel: Int
+) -> String {
+    let expectedMirror = Mirror(reflecting: expected)
+    let receivedMirror = Mirror(reflecting: received)
+
+    let receivedPrintable: String
+    let expectedPrintable: String
+    if receivedMirror.children.count == 0, expectedMirror.children.count != 0 {
+        receivedPrintable = String(dumping: received)
+        expectedPrintable = enumLabelFromFirstChild(expectedMirror) ?? String(describing: expected)
+    } else if expectedMirror.children.count == 0, receivedMirror.children.count != 0 {
+        receivedPrintable = enumLabelFromFirstChild(receivedMirror) ?? String(describing: received)
+        expectedPrintable = String(dumping: expected)
+    } else {
+        receivedPrintable = String(describing: received)
+        expectedPrintable = String(describing: expected)
+    }
+    return "R7" + generateExpectedReceiveBlock(expectedPrintable, receivedPrintable, indentationLevel)
 }
 
 /// Compares 2 objects and iterates over their differences
@@ -22,26 +59,29 @@ fileprivate extension String {
 ///   - rhs: received object
 ///   - closure: iteration closure
 fileprivate func diff<T>(_ expected: T, _ received: T, level: Int = 0, closure: (_ description: String) -> Void) {
-    let lhsMirror = Mirror(reflecting: expected)
-    let rhsMirror = Mirror(reflecting: received)
+    let expectedMirror = Mirror(reflecting: expected)
+    let receivedMirror = Mirror(reflecting: received)
 
-    guard lhsMirror.children.count != 0, rhsMirror.children.count != 0 else {
+    guard expectedMirror.children.count != 0, receivedMirror.children.count != 0 else {
         if String(dumping: received) != String(dumping: expected) {
-            closure("received: \"\(received)\" expected: \"\(expected)\"\n")
+            closure(handleChildlessEnum(expected, received, level))
         }
         return
     }
 
-    let hasDiffNumOfChildren = lhsMirror.children.count != rhsMirror.children.count
-    switch (lhsMirror.displayStyle, rhsMirror.displayStyle) {
+    let hasDiffNumOfChildren = expectedMirror.children.count != receivedMirror.children.count
+    switch (expectedMirror.displayStyle, receivedMirror.displayStyle) {
     case (.collection?, .collection?) where hasDiffNumOfChildren,
          (.dictionary?, .dictionary?) where hasDiffNumOfChildren,
          (.set?, .set?) where hasDiffNumOfChildren:
-            closure("""
-                different count:
-                \(indentation(level: level))received: \"\(received)\" (\(rhsMirror.children.count))
-                \(indentation(level: level))expected: \"\(expected)\" (\(lhsMirror.children.count))\n
-                """)
+        let expectedPrintable = "(\(expectedMirror.children.count)) \(expected)"
+        let receivedPrintable = "(\(receivedMirror.children.count)) \(received)"
+        let header = "\(indentation(level: level))Different count:\n"
+        closure(
+            header
+                + "R1"
+                + generateExpectedReceiveBlock(expectedPrintable, receivedPrintable, level + 1)
+        )
         return
     case (.dictionary?, .dictionary?):
         if let expectedDict = expected as? Dictionary<AnyHashable, Any>,
@@ -52,7 +92,7 @@ fileprivate func diff<T>(_ expected: T, _ received: T, level: Int = 0, closure: 
                     results.append(diff)
                 }
                 if !results.isEmpty {
-                    closure("child key \(key.description):\n\(indentation(level: max(level + 1, 1)))" + results.joined(separator: "\n\(indentation(level: max(level + 1, 1)))"))
+                    closure("R2 Child key \(key.description):\n\(indentation(level: max(level + 1, 1)))" + results.joined(separator: "\n\(indentation(level: max(level + 1, 1)))"))
                 }
             }
             return
@@ -65,42 +105,70 @@ fileprivate func diff<T>(_ expected: T, _ received: T, level: Int = 0, closure: 
 
             var results = [String]()
             uniqueExpected.forEach { unique in
-                results.append("SetElement missing: \(unique.description)\n")
+                results.append("R3 SetElement missing: \(unique.description)\n")
             }
 
             if !uniqueExpected.isEmpty {
-                closure(results.joined(separator: "\(indentation(level: max(level, 1)))"))
+                closure(results.joined(separator: "\(indentation(level: max(level + 1, 1)))"))
             }
             return
         }
-    case (.enum?, .enum?) where lhsMirror.children.first?.label != rhsMirror.children.first?.label,
-         (.optional?, .optional?) where hasDiffNumOfChildren:
-        closure("received: \"\(received)\" expected: \"\(expected)\"\n")
+    case (.enum?, .enum) where hasDiffNumOfChildren:
+        closure("""
+            R4 Different count:
+            \(indentation(level: level))Received: \(received) (\(receivedMirror.children.count))
+            \(indentation(level: level))Expected: \(expected) (\(expectedMirror.children.count))\n
+            """)
+        return
+    case (.enum?, .enum?) where expectedMirror.children.first?.label != receivedMirror.children.first?.label:
+        let expectedPrintable = expectedMirror.children.first?.label ?? "UNKNOWN"
+        let receivedPrintable = receivedMirror.children.first?.label ?? "UNKNOWN"
+
+        closure("R5" + generateExpectedReceiveBlock(expectedPrintable, receivedPrintable, level))
+        return
+//        closure("\(indentation(level: level))R5 Received: \(received) Expected: \(expected)\n")
+
+//    case (.optional?, .optional?) where hasDiffNumOfChildren:
+
     default:
         break
     }
 
-    let zipped = zip(lhsMirror.children, rhsMirror.children)
+    let zipped = zip(expectedMirror.children, receivedMirror.children)
     zipped.forEach { (lhs, rhs) in
         let leftDump = String(dumping: lhs.value)
         if leftDump != String(dumping: rhs.value) {
-            if let notPrimitive = Mirror(reflecting: lhs.value).displayStyle, notPrimitive != .tuple {
+            if let notPrimitive = Mirror(reflecting: lhs.value).displayStyle/*, notPrimitive != .tuple*/ {
                 var results = [String]()
                 diff(lhs.value, rhs.value, level: level + 1) { diff in
                     results.append(diff)
                 }
                 if !results.isEmpty {
-                    closure("child \(lhs.label ?? ""):\n\(indentation(level: level + 1))" + results.joined(separator: "\(indentation(level: level + 1))"))
+                    closure("R8\(indentation(level: level))\(expectedMirror.displayStyleDescriptor) \(lhs.label ?? ""):\n" + results.joined())
                 }
-            } else {
-                closure("\(lhs.label ?? "") received: \"\(rhs.value)\" expected: \"\(lhs.value)\"\n")
+            } else { // todo maybe remove
+                closure("R9\(expectedMirror.displayStyleDescriptor) \(lhs.label ?? ""):\n" + "R6" + generateExpectedReceiveBlock(String(describing: lhs.value), String(describing: rhs.value), level + 1))
+//                closure("\(lhs.label ?? "") received: \(rhs.value) expected: \(lhs.value)\n")
             }
         }
     }
 }
 
+private func generateExpectedReceiveBlock(
+    _ expected: String,
+    _ received: String,
+    _ indentationLevel: Int
+) -> String {
+    let indentationSpacing = indentation(level: indentationLevel)
+    return """
+    \(indentationSpacing)Received: \(received)
+    \(indentationSpacing)Expected: \(expected)
+    """
+    //"\(indentationSpacing)Received: \(received)\n\(indentationSpacing)Expected: \(expected)\n"
+}
+
 private func indentation(level: Int) -> String {
-    return (0..<level).reduce("") { acc, _ in acc + "\t" }
+    return (0..<level).reduce("") { acc, _ in acc + "|\t" }
 }
 
 /// Builds list of differences between 2 objects
@@ -137,3 +205,8 @@ public func dumpDiff<T: Equatable>(_ expected: T, _ received: T) {
 public func dumpDiff<T>(_ expected: T, _ received: T) {
     diff(expected, received).forEach { print($0) }
 }
+
+// TO CHECK:
+//DONE Enum with different contents
+// Enum with different label
+// Enum with nil, rather than a value
--- Tests/DifferenceTests/DifferenceTests.swift
+++ Tests/DifferenceTests/DifferenceTests.swift
@@ -37,13 +37,20 @@ private enum State {
     case anotherLoaded([Int])
 }
 
+private func dumpDiffSurround<T: Equatable>(_ lhs: T, _ rhs: T) {
+    print("====START DIFF====")
+    dumpDiff(lhs, rhs)
+    print("=====END DIFF=====")
+}
+
 class DifferenceTests: XCTestCase {
 
     func testCanFindRootPrimitiveDifference() {
+//        dumpDiffSurround(2, 3)
         let results = diff(2, 3)
 
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "received: \"3\" expected: \"2\"\n")
+        XCTAssertEqual(results.first, "R7Received: 3\nExpected: 2")
     }
 
     fileprivate let truth = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
@@ -51,199 +58,189 @@ class DifferenceTests: XCTestCase {
     func testCanFindPrimitiveDifference() {
         let stub = Person(name: "Krzysztof", age: 30, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
 
+//        dumpDiffSurround(truth, stub)
         let results = diff(truth, stub)
 
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "age received: \"30\" expected: \"29\"\n")
+        XCTAssertEqual(results.first, "R9Child age:\nR6|\tReceived: 30\n|\tExpected: 29")
 
     }
 
     func testCanFindMultipleDifference() {
         let stub = Person(name: "Adam", age: 30, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
 
+//        dumpDiffSurround(truth, stub)
         let results = diff(truth, stub)
 
         XCTAssertEqual(results.count, 2)
-        XCTAssertEqual(results.first, "name received: \"Adam\" expected: \"Krzysztof\"\n")
-        XCTAssertEqual(results.last, "age received: \"30\" expected: \"29\"\n")
+        XCTAssertEqual(results.first, "R9Child name:\nR6|\tReceived: Adam\n|\tExpected: Krzysztof")
+        XCTAssertEqual(results.last, "R9Child age:\nR6|\tReceived: 30\n|\tExpected: 29")
     }
 
     func testCanFindComplexDifference() {
         let stub = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "2nd Street", postCode: "00-1000", counter: .init(counter: 1)), pet: nil)
 
+        dumpDiffSurround(truth, stub)
         let results = diff(truth, stub)
-        dumpDiff(truth, stub)
 
         XCTAssertEqual(results.count, 1)
         XCTAssertEqual(results.first, "child address:\nstreet received: \"2nd Street\" expected: \"Times Square\"\nchild counter:\n\tcounter received: \"1\" expected: \"2\"\n")
 
     }
-
-    func testCanGiveDescriptionForOptionalOnLeftSide() {
-        let truth = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
-
-        let stub = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: .init(name: "Fluffy"))
-
-        let results = diff(truth, stub)
-        XCTAssertEqual(results.count, 1)
-    }
-
-    func testCanGiveDescriptionForOptionalOnRightSide() {
-        let truth = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: .init(name: "Fluffy"))
-
-        let stub = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
-
-        let results = diff(truth, stub)
-        XCTAssertEqual(results.count, 1)
-    }
-
-    func test_canFindCollectionCountDifference() {
-        let results = diff([1], [1, 3])
-
-        XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "different count:\nreceived: \"[1, 3]\" (2)\nexpected: \"[1]\" (1)\n")
-    }
-
-    func test_canFindEnumCaseDifferenceWhenAssociatedValuesAreIdentical() {
-        let results = diff(State.loaded([0]), State.anotherLoaded([0]))
-
-        XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "received: \"anotherLoaded([0])\" expected: \"loaded([0])\"\n")
-    }
-
-    func test_canFindDictionaryCountDifference() {
-        let results = diff(["A": "B"], [:])
-
-        XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "received: \"[:]\" expected: \"[\"A\": \"B\"]\"\n")
-    }
-
-    func test_canFindOptionalDifferenceBetweenSomeAndNone() {
-        let results = diff(["A": "B"], nil)
-
-        XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results.first, "received: \"nil\" expected: \"Optional([\"A\": \"B\"])\"\n")
-    }
-
-    func test_canFindDictionaryDifference() {
-        let results = diff(
-            [
-                "a": 1,
-                "b": 3,
-                "c": 3,
-                "d": 4,
-            ],
-            [
-                "a": 1,
-                "b": 2,
-                "c": 3,
-                "d": 0,
-            ]
-        )
-
-        // TODO: Should results.count be 2?
-        XCTAssertEqual(results.count, 2)
-        XCTAssertEqual(results.first, "")
-    }
-
-    func test_set() {
-        (0..<1000).forEach { _ in
-            let expected: Set<Int> = [1, 2, 3, 4, 5]
-            let actual: Set<Int> = [7, 6, 5, 4, 3]
-
-            let results = diff(expected, actual)
-
-            XCTAssertEqual(results.count, 1)
-                // Need to figure out how to get consistent ordering of a set in the result. Alternately, reduce this test to only 1 diff.
-            XCTAssertEqual(results.first!, "Set mismatch:\n\tvalue received: \"7\" expected: \"2\"\n\tvalue received: \"6\" expected: \"1\"\n" )
-        }
-    }
-
-    func test_inner_set() {
-        let expectedAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [1, 2, 3, 4, 5], dictionaryOfInts: ["a":1])
-        let actualAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [3, 4, 5, 6, 7], dictionaryOfInts: ["a":2])
-
-        let newPersonExpected = NewPerson(name: "Krzysztof", age: 29, address: expectedAddress, secondAddress: expectedAddress, pet: nil)
-        let newPersonActual = NewPerson(name: "Krzysztof", age: 29, address: actualAddress, secondAddress: actualAddress, pet: nil)
-
-        let results = diff(newPersonExpected, newPersonActual)
-        dumpDiff(newPersonExpected, newPersonActual)
-
-        XCTAssertEqual(results.first!, "" )
-        print("@@@@@@@@@@@@")
-        print(results.first!)
-        print("@@@@@@@@@@@@")
-     }
-
-    func test_inner_dict() {
-        let expectedDicts = [
-            "a": 1,
-            "b": 3,
-            "c": 3,
-            "d": 4,
-        ]
-        let actualDicts = [
-            "a": 1,
-            "b": 2,
-            "c": 6,
-            "d": 0,
-        ]
-
-        let expectedAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [], dictionaryOfInts: expectedDicts)
-        let actualAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [], dictionaryOfInts: actualDicts)
-
-        let newPersonExpected = NewPerson(name: "Krzysztof", age: 29, address: expectedAddress, secondAddress: expectedAddress, pet: nil)
-        let newPersonActual = NewPerson(name: "Krzysztof", age: 29, address: actualAddress, secondAddress: actualAddress, pet: nil)
-
-        let results = diff(newPersonExpected, newPersonActual)
-
-        dumpDiff(newPersonExpected, newPersonActual)
-        XCTAssertEqual(results.first!, "" )
-        print(results.joined(separator: "\n"))
-
-     }
-
-    func test_multiple_child_failures() {
-        let expectedContainer = Container1(
-            topValue: 1,
-            container2: .init(
-                value: 2,
-                container3: .init(
-                    value: 3,
-                    container4: .init(value: 4)
-                )
-            )
-        )
-
-        let actualContainer = Container1(
-            topValue: -1,
-            container2: .init(
-                value: -2,
-                container3: .init(
-                    value: -3,
-                    container4: .init(value: -4)
-                )
-            )
-        )
-
-        let results = diff(expectedContainer, actualContainer)
-
-        print("@@@@@@@@@@@@")
-        print(results.joined(separator: "\n"))
-        print("@@@@@@@@@@@@")
-    }
 //
-//    static var allTests = [
-//        ("testCanFindRootPrimitiveDifference", testCanFindRootPrimitiveDifference),
-//        ("testCanFindPrimitiveDifference", testCanFindPrimitiveDifference),
-//        ("testCanFindMultipleDifference", testCanFindMultipleDifference),
-//        ("testCanFindComplexDifference", testCanFindComplexDifference),
-//        ("test_canFindCollectionCountDifference", test_canFindCollectionCountDifference),
-//        ("test_canFindEnumCaseDifferenceWhenAssociatedValuesAreIdentical", test_canFindEnumCaseDifferenceWhenAssociatedValuesAreIdentical),
-//        ("test_canFindDictionaryCountDifference", test_canFindDictionaryCountDifference),
-//        ("test_canFindOptionalDifferenceBetweenSomeAndNone", test_canFindOptionalDifferenceBetweenSomeAndNone),
-//        ("test_canFindDictionaryDifference", test_canFindDictionaryDifference)
-//    ]
+//    func testCanGiveDescriptionForOptionalOnLeftSide() {
+//        let truth = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
+//
+//        let stub = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: .init(name: "Fluffy"))
+//
+//        let results = diff(truth, stub)
+//        XCTAssertEqual(results.count, 1)
+//    }
+//
+//    func testCanGiveDescriptionForOptionalOnRightSide() {
+//        let truth = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: .init(name: "Fluffy"))
+//
+//        let stub = Person(name: "Krzysztof", age: 29, address: Person.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2)), pet: nil)
+//
+//        let results = diff(truth, stub)
+//        XCTAssertEqual(results.count, 1)
+//    }
+
+//    func test_canFindCollectionCountDifference() {
+//        let results = diff([1], [1, 3])
+//
+//        XCTAssertEqual(results.count, 1)
+//        XCTAssertEqual(results.first, "different count:\nreceived: \"[1, 3]\" (2)\nexpected: \"[1]\" (1)\n")
+//    }
+//
+//    func test_canFindEnumCaseDifferenceWhenAssociatedValuesAreIdentical() {
+//        let results = diff(State.loaded([0]), State.anotherLoaded([0]))
+//
+//        XCTAssertEqual(results.count, 1)
+//        XCTAssertEqual(results.first, "received: \"anotherLoaded([0])\" expected: \"loaded([0])\"\n")
+//    }
+//
+//    func test_canFindDictionaryCountDifference() {
+//        let results = diff(["A": "B"], [:])
+//
+//        XCTAssertEqual(results.count, 1)
+//        XCTAssertEqual(results.first, "received: \"[:]\" expected: \"[\"A\": \"B\"]\"\n")
+//    }
+//
+//    func test_canFindOptionalDifferenceBetweenSomeAndNone() {
+//        let results = diff(["A": "B"], nil)
+//
+//        XCTAssertEqual(results.count, 1)
+//        XCTAssertEqual(results.first, "received: \"nil\" expected: \"Optional([\"A\": \"B\"])\"\n")
+//    }
+//
+//    func test_canFindDictionaryDifference() {
+//        let results = diff(
+//            [
+//                "a": 1,
+//                "b": 3,
+//                "c": 3,
+//                "d": 4,
+//            ],
+//            [
+//                "a": 1,
+//                "b": 2,
+//                "c": 3,
+//                "d": 0,
+//            ]
+//        )
+//
+//        // TODO: Should results.count be 2?
+//        XCTAssertEqual(results.count, 2)
+//        XCTAssertEqual(results.first, "")
+//    }
+//
+//    func test_set() {
+//        (0..<1000).forEach { _ in
+//            let expected: Set<Int> = [1, 2, 3, 4, 5]
+//            let actual: Set<Int> = [7, 6, 5, 4, 3]
+//
+//            let results = diff(expected, actual)
+//
+//            XCTAssertEqual(results.count, 1)
+//                // Need to figure out how to get consistent ordering of a set in the result. Alternately, reduce this test to only 1 diff.
+//            XCTAssertEqual(results.first!, "Set mismatch:\n\tvalue received: \"7\" expected: \"2\"\n\tvalue received: \"6\" expected: \"1\"\n" )
+//        }
+//    }
+//
+//    func test_inner_set() {
+//        let expectedAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [1, 2, 3, 4, 5], dictionaryOfInts: ["a":1])
+//        let actualAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [3, 4, 5, 6, 7], dictionaryOfInts: ["a":2])
+//
+//        let newPersonExpected = NewPerson(name: "Krzysztof", age: 29, address: expectedAddress, secondAddress: expectedAddress, pet: nil)
+//        let newPersonActual = NewPerson(name: "Krzysztof", age: 29, address: actualAddress, secondAddress: actualAddress, pet: nil)
+//
+//        let results = diff(newPersonExpected, newPersonActual)
+//        dumpDiff(newPersonExpected, newPersonActual)
+//
+//        XCTAssertEqual(results.first!, "" )
+//        print("@@@@@@@@@@@@")
+//        print(results.first!)
+//        print("@@@@@@@@@@@@")
+//     }
+//
+//    func test_inner_dict() {
+//        let expectedDicts = [
+//            "a": 1,
+//            "b": 3,
+//            "c": 3,
+//            "d": 4,
+//        ]
+//        let actualDicts = [
+//            "a": 1,
+//            "b": 2,
+//            "c": 6,
+//            "d": 0,
+//        ]
+//
+//        let expectedAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [], dictionaryOfInts: expectedDicts)
+//        let actualAddress = NewPerson.Address(street: "Times Square", postCode: "00-1000", counter: .init(counter: 2), setOfInts: [], dictionaryOfInts: actualDicts)
+//
+//        let newPersonExpected = NewPerson(name: "Krzysztof", age: 29, address: expectedAddress, secondAddress: expectedAddress, pet: nil)
+//        let newPersonActual = NewPerson(name: "Krzysztof", age: 29, address: actualAddress, secondAddress: actualAddress, pet: nil)
+//
+//        let results = diff(newPersonExpected, newPersonActual)
+//
+//        dumpDiff(newPersonExpected, newPersonActual)
+//        XCTAssertEqual(results.first!, "" )
+//        print(results.joined(separator: "\n"))
+//
+//     }
+//
+//    func test_multiple_child_failures() {
+//        let expectedContainer = Container1(
+//            topValue: 1,
+//            container2: .init(
+//                value: 2,
+//                container3: .init(
+//                    value: 3,
+//                    container4: .init(value: 4)
+//                )
+//            )
+//        )
+//
+//        let actualContainer = Container1(
+//            topValue: -1,
+//            container2: .init(
+//                value: -2,
+//                container3: .init(
+//                    value: -3,
+//                    container4: .init(value: -4)
+//                )
+//            )
+//        )
+//
+//        let results = diff(expectedContainer, actualContainer)
+//
+//        print("@@@@@@@@@@@@")
+//        print(results.joined(separator: "\n"))
+//        print("@@@@@@@@@@@@")
+//    }
 }
 
 fileprivate struct Container1: Equatable {
